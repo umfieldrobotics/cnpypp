b0a8c4b65f1af7385d5b799378e9e21829be94d4
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2022-08-15 19:21:18 +0200

    add example with range-v3 zip()

---
+++ range_zip_example.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+
+#include <cnpy++.hpp>
+
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/transform.hpp>
+#include <range/v3/view/zip.hpp>
+
+int main() {
+  auto const i = ranges::views::ints(1, 10);
+  auto const sq = i | ranges::views::transform([](auto x) { return x * x; });
+  auto const trip =
+      i | ranges::views::transform([](auto x) { return x * x * x; });
+
+  auto const z = ranges::views::zip(i, sq, trip);
+  cnpypp::npy_save("structured.npy", {"a", "b", "c"}, z.begin(), {z.size()});
+
+  return EXIT_SUCCESS;
+}
