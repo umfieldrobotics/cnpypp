7cd2914ec7d315e51dbf73e08d10f70a4bb11737
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2022-08-16 02:08:13 +0200

    add range example

---
+++ range_example.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <ranges>
+
+#include <cnpy++.hpp>
+
+int main() {
+  auto const r = std::ranges::iota_view{1, 11};
+  
+  cnpypp::npy_save("range_example.npy", r.begin(), {r.size()}, "w", cnpypp::MemoryOrder::Fortran);
+  
+  return EXIT_SUCCESS;
+}
