028b2a4cfe77aab3370e1db6373931e62aba308c
Maximilian Reininghaus <maximilian.reininghaus@kit.edu>
2022-08-17 18:32:20 +0200

    better output filename

--- range_example.cpp
+++ range_example.cpp
@@ -5,8 +5,9 @@
 
 int main() {
   auto const r = std::ranges::iota_view{1, 11};
-  
-  cnpypp::npy_save("range_example.npy", r.begin(), {r.size()}, "w", cnpypp::MemoryOrder::Fortran);
-  
+
+  cnpypp::npy_save("range_data.npy", r.begin(), {r.size()}, "w",
+                   cnpypp::MemoryOrder::Fortran);
+
   return EXIT_SUCCESS;
 }
