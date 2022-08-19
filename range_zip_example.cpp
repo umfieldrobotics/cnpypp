0dc24dd7383b2f1a6bba2660745c74d90007cf30
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2022-08-19 20:47:15 +0200

    clang-format

--- range_zip_example.cpp
+++ range_zip_example.cpp
@@ -7,13 +7,14 @@
 #include <range/v3/view/zip.hpp>
 
 int main() {
-  auto const i = ranges::views::ints(1, 10);
+  auto const i = ranges::views::ints(1, 1024 * 1024 * 1024);
   auto const sq = i | ranges::views::transform([](auto x) { return x * x; });
   auto const trip =
       i | ranges::views::transform([](auto x) { return x * x * x; });
 
   auto const z = ranges::views::zip(i, sq, trip);
-  cnpypp::npy_save("range_zip_data.npy", {"a", "b", "c"}, z.begin(), {z.size()});
+  cnpypp::npy_save("range_zip_data.npy", {"a", "b", "c"}, z.begin(),
+                   {z.size()});
 
   return EXIT_SUCCESS;
 }
