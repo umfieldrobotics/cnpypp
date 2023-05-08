9c349409ae1dd38917d1fdd44ce7634b324e0e25
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2023-05-08 18:38:35 +0200

    re-enable range_example

--- examples/range_example.cpp
+++ examples/range_example.cpp
@@ -6,8 +6,8 @@
 int main() {
   auto const r = std::ranges::iota_view{1, 11};
 
-//  cnpypp::npy_save("range_data.npy", r.begin(), {r.size()}, "w",
-//                   cnpypp::MemoryOrder::Fortran);
+  cnpypp::npy_save("range_data.npy", r.begin(), {r.size()}, "w",
+                   cnpypp::MemoryOrder::Fortran);
 
   return EXIT_SUCCESS;
 }
